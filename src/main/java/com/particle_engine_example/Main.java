b36a9095251ee7e16dddb9d5ebeab0fa32b63524
SoKnox <sophieknox1202@gmail.com>
2024-09-10 16:34:20 -0500

    first commit

---
+++ src/main/java/com/particle_engine_example/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Sophie Knox
+ * 
+ * Particle Engine 
+ * litterly just doing this to commit
+ */
+
+
+package com.particle_engine_example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
